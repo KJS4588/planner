baf9da7c64fca8d9b9b83865a71888cc87ee9573
Hyeonbeen <gusqls1124@gmail.com>
2020-07-16 19:00:31 +0900

    Check cloud_filterd data

--- src/clustering.cpp
+++ src/clustering.cpp
@@ -72,7 +72,48 @@ void Planner::pointCallback(const sensor_msgs::PointCloud2ConstPtr &input){
     ec.setSearchMethod(tree);
     ec.setInputCloud(cloud_filterd);
     ec.extract(cluster_indices);
+
+
+    cout << "#########################################" << endl;
+
+    for (vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it) {
+
+        double sum_x = 0.0;
+        double sum_y = 0.0;
+        double sum_z = 0.0;
+
+        double count = 0.0;
+
+        for (vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit) {
+            PointType pt2;
+            pt2.x = cloud_filterd->points[*pit].x, pt2.y = cloud_filterd->points[*pit].y, pt2.z = cloud_filterd->points[*pit].z;
+
+            sum_x += pt2.x;
+            sum_y += pt2.y;
+            sum_z += pt2.z;
+
+            count += 1.0;
+        }
+
+        // use it_size!
+
+        double center_x = sum_x/count;
+        double center_y = sum_y/count;
+        double center_z = sum_z/count;
+
+        cout << endl;
+        cout << "center_X = " << center_x << endl;
+        cout << "center_Y = " << center_y << endl;
+        cout << "center_Z = " << center_z << endl;
+        cout << endl;
+
+        visualizePoint(center_x, center_y, center_z);
+    }
+
+    cout << "#########################################" << endl;
+
     
+	/*
     pcl::PointCloud<PointType> Result_cloud;
     cout << "Number of clusters is equal to " << cluster_indices.size() << endl;
     int j = 0;
@@ -101,7 +142,7 @@ void Planner::pointCallback(const sensor_msgs::PointCloud2ConstPtr &input){
     sensor_msgs::PointCloud2 result;
     pcl_conversions::fromPCL(cloud_p, result);
     result.header.frame_id = "velodyne";
-    pub_.publish(result);
+    pub_.publish(result);*/
 
 }
 int main(int argc, char **argv){
